71f91c8ac63cfabe5e474af562b027fbd80226e8
Sergio Alvarez <salvare736@gmail.com>
2021-05-04 17:35:30 -0400

    PUT post operation is working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -97,7 +97,23 @@ router.delete('/:id', (req, res) => {
 
 router.put('/:id', (req, res) => {
     Posts.update(req.params.id, req.body)
-        .then()
+        .then(updatedPosts => {
+            if (!updatedPosts) {
+                res.status(404).json({
+                    message: 'The post with the specified ID does not exist'
+                });
+            } else {
+                if (!req.body.title || !req.body.contents) {
+                    res.status(400).json({
+                        message: 'Please provide title and contents for the post'
+                    });
+                } else {
+                    res.status(200).json({
+                        message: `${updatedPosts} post was updated`
+                    });
+                }
+            }
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json({
